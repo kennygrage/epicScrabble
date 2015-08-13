a7aaf70fe9b72cf84c8b36a74210461641a2a534
kg <student@epicodus.com>
2015-08-13 11:47:10 -0700

    continue app.php

--- app/app.php
+++ app/app.php
@@ -8,7 +8,20 @@
     $app = new Silex\Application();
     $app['debug'] = true;
 
-    $app->register(new Silex\Provider\TwigServiceProvider(), array (
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/view_results", function() use ($app) {
+        $scrabble = new ScrabbleCalc;
+        $value = $scrabble->calcValue($_GET['word']);
+
+        return $app['twig']->render('result.html.twig', array('value' => $value));
+    });
+
+    return $app;
 ?>
